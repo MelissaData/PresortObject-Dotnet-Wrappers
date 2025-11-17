1e68adfd19f34128841263e1d34e19dba54681ea
Melissa <>
2025-11-17 10:20:30 -0800

    Tested successfully using Melissa object for 2025.10

--- BuildPresortNETdll.ps1
+++ BuildPresortNETdll.ps1
@@ -18,7 +18,7 @@ class FileConfig {
 
 ######################### Config ###########################
 
-$RELEASE_VERSION = '2025.08'
+$RELEASE_VERSION = '2025.10'
 
 # Uses the location of the .ps1 file 
 $CurrentPath = $PSScriptRoot
